fe618754b7a76c75061c55150c300c79588aaf00
Daniel Wymark <d.d.wymark@gmail.com>
2026-09-22 02:42:17 -0700

    tests: cover single-row state gathers and an odd-width tail

--- tests/test-backend-ops.cpp
+++ tests/test-backend-ops.cpp
@@ -8505,6 +8505,9 @@ static std::vector<std::unique_ptr<test_case>> make_test_cases_eval() {
     }
 
     test_cases.emplace_back(new test_get_rows(GGML_TYPE_F32, 1, 8, 2, 1, 1, false));
+    for (int n : {127, 30720, 786432}) {
+        test_cases.emplace_back(new test_get_rows(GGML_TYPE_F32, n, 1, 1, 1, 1, false));
+    }
     for (ggml_type type : all_types) {
         for (int b : {1, 7}) {
             for (bool v : {false, true}) {
